c073781ad18c2ce65464a18ca2d645cadaa97358
Ken Tominaga <ken.for.develop@gmail.com>
2014-12-10 22:59:51 +0900

    Try to get angle of nodes

--- seminar2014/GameViewController.swift
+++ seminar2014/GameViewController.swift
@@ -13,6 +13,7 @@ import Alamofire
 
 class GameViewController: UIViewController, SRWebSocketDelegate {
     
+    // var sceneView: SCNView?
     var scene: SCNScene?
     var arrowImageView: UIImageView?
     let w: CGFloat = 320.0
@@ -21,152 +22,95 @@ class GameViewController: UIViewController, SRWebSocketDelegate {
     let IPADDRESS = "172.16.0.2"
     let PORT = "443"
     
-    var webSocketInstance: SRWebSocket?
+    var webSocketInstance: SRWebSocket = SRWebSocket()
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        self.view.backgroundColor = UIColor.blackColor()
         
+        self.scene = SCNScene(named: "Armv0.dae")
         
-        // setupArrow()
+        let camera = SCNNode()
+        camera.camera = SCNCamera()
+        self.scene?.rootNode.addChildNode(camera)
+        camera.position = SCNVector3(x: 0, y: 0, z: 15)
         
-        setupScene()
-        setupCamera()
-        setupArms()
+        let robo = self.scene!.rootNode.childNodeWithName("Robo", recursively: true)!
+        robo.rotation = SCNVector4Make(0, 0, 1, Float(M_PI)/2.0)
+        let rootBone = robo.childNodeWithName("RootBone", recursively: true)!
+        let handBone = robo.childNodeWithName("HandBone", recursively: true)
+        
+        let ikc = SCNIKConstraint.inverseKinematicsConstraintWithChainRootNode(rootBone)
+        ikc.influenceFactor = 1.0
+        handBone?.constraints = [ikc]
+        
+        SCNTransaction.begin()
+        SCNTransaction.setAnimationDuration(1.0)
+        ikc.targetPosition = self.scene!.rootNode.convertPosition(SCNVector3(x: 0, y: -4.2, z: 0), toNode: nil)
+        SCNTransaction.commit()
+        
+        let sceneView = self.view as SCNView
+        sceneView.scene = self.scene
+        sceneView.backgroundColor = UIColor.whiteColor()
+        
+//        let box = SCNBox(width: 9.4, height: 16.7, length: 1, chamferRadius: 0)
+//        box.firstMaterial?.diffuse.contents = UIColor.redColor()
+//        let boxNode = SCNNode(geometry: box)
+//        self.scene?.rootNode.addChildNode(boxNode)
         
         let url = NSURL(string: "ws://\(IPADDRESS):\(PORT)")!
-        println(url)
-        webSocketInstance = SRWebSocket(URLRequest: NSURLRequest(URL: url))
-        webSocketInstance?.delegate = self
-        webSocketInstance?.open()
-        // Alamofire.request(.POST, url)
+        webSocketInstance = SRWebSocket(URLRequest: NSURLRequest(URL: url))!
+        webSocketInstance.delegate = self
+        webSocketInstance.open()
     }
     
     // MARK: SRWebSocketDelegate
     
     func webSocketDidOpen(webSocket: SRWebSocket!) {
-        let parameters = [
-            "radian1": 20 * M_PI / 180,
-            "radian2": 30 * M_PI / 180
-        ]
+        println("socket open!")
         
-        let data = "{\"radian1\":\"\(20 * M_PI / 180)\", \"radian2\":\"\(30 * M_PI / 180)\"}"
+        let upperBone = self.scene!.rootNode.childNodeWithName("UpperBone", recursively: true)!
+        let foreBone = self.scene!.rootNode.childNodeWithName("ForeBone", recursively: true)!
+        
+        println(upperBone.rotation)
+        println(foreBone.rotation)
+        
+        let radian1 = 0
+        let radian2 = 0
+        
+        let data = "{\"radian1\":\"\(radian1)\", \"radian2\":\"\(radian2)\"}"
         
         webSocket.send(data)
-
     }
     
     func webSocket(webSocket: SRWebSocket!, didReceiveMessage message: AnyObject!) {
         println("data received")
     }
     
-    func setupScene() {
-        let sv = SCNView(frame: CGRect(x: 0, y: 0, width: w, height: h))
-        sv.scene = SCNScene()
-        sv.backgroundColor = UIColor.whiteColor()
-        self.view.addSubview(sv)
-        
-        self.scene = sv.scene
-    }
-    
-    func setupArms() {
-        let anchor = SCNSphere(radius: 5)
-        anchor.firstMaterial?.diffuse.contents = UIColor.blackColor()
-        let anchorNode = SCNNode(geometry: anchor)
-        anchorNode.name = "anchor"
-        anchorNode.position = SCNVector3(x: 0, y: 0, z: 0)
-        anchorNode.physicsBody = SCNPhysicsBody.kinematicBody()
-        
-        let arm1 = SCNCylinder(radius: 5, height: 20)
-        arm1.firstMaterial?.diffuse.contents = UIColor.blueColor()
-        let armNode1 = SCNNode(geometry: arm1)
-        armNode1.name = "arm1"
-        armNode1.position = SCNVector3(x: 0, y: -10, z: 0)
-        armNode1.physicsBody = SCNPhysicsBody.kinematicBody()
-        
-        let arm2 = SCNCylinder(radius: 5, height: 20)
-        arm2.firstMaterial?.diffuse.contents = UIColor.redColor()
-        let armNode2 = SCNNode(geometry: arm2)
-        armNode2.name = "arm2"
-        armNode2.position = SCNVector3(x: 0, y: -20, z: 0)
-        armNode2.physicsBody = SCNPhysicsBody.kinematicBody()
-        
-        let hand = SCNBox(width: 10, height: 10, length: 10, chamferRadius: 0)
-        hand.firstMaterial?.diffuse.contents = UIColor.yellowColor()
-        let handNode = SCNNode(geometry: hand)
-        handNode.name = "hand"
-        handNode.position = SCNVector3(x: 0, y: -15, z: 0)
-        handNode.physicsBody = SCNPhysicsBody.kinematicBody()
-        
-        // let j1 = SCNPhysicsBallSocketJoint(
-        
-        self.scene?.rootNode.addChildNode(anchorNode)
-        anchorNode.addChildNode(armNode1)
-        armNode1.addChildNode(armNode2)
-        armNode2.addChildNode(handNode)
-        
-//        let ik = SCNIKConstraint.inverseKinematicsConstraintWithChainRootNode(anchorNode)
-//        handNode.constraints = [ik]
-//        SCNTransaction.setAnimationDuration(5.0)
-//        ik.targetPosition = SCNVector3(x: 0, y: 50, z: 0)
-    }
-    
-    func setupCamera() {
-        let camera = SCNNode()
-        camera.camera = SCNCamera()
-//        camera.position = SCNVector3(x: 0, y: 20, z: 100)
-//        camera.rotation = SCNVector4(x: 0, y: 0, z: 0, w: -0.1)
-        camera.position = SCNVector3(x: 15, y: 0, z: 100)
-        camera.rotation = SCNVector4(x: 0, y: 0, z: 0, w: -0.1)
-        self.scene?.rootNode.addChildNode(camera)
-    }
-    
-//    // MARK: arrow
-//    
-//    func setupArrow() {
-//        let baseView = UIView(frame: CGRect(x: 0, y: 0, width: w, height: h - w))
-//        baseView.backgroundColor = UIColor.yellowColor()
-//        baseView.userInteractionEnabled = true
-//        
-//        arrowImageView = UIImageView(frame: CGRect(origin: CGPoint(x: 0, y: 50), size: CGSize(width: w, height: (h - w) / 2)))
-//        arrowImageView?.image = UIImage(named: "arrow")
-//        arrowImageView?.userInteractionEnabled = true
-//        arrowImageView?.contentMode = UIViewContentMode.ScaleAspectFit
-//        baseView.addSubview(arrowImageView!)
-//        
-//        let pgr = UIPanGestureRecognizer(target: self, action: Selector("didDrag:"))
-//        baseView.addGestureRecognizer(pgr)
-//        
-//        self.view.addSubview(baseView)
-//    }
-//    
-//    func didDrag(pgr: UIPanGestureRecognizer) {
-//        let targetView = pgr.view!
-//        let position = pgr.locationInView(targetView)
-//        
-//        let dx = position.x - targetView.center.x
-//        let dy = position.y - targetView.frame.origin.y
-//        let angle = -atan(dx / dy)
-//        arrowImageView?.transform = CGAffineTransformMakeRotation(angle)
-//    }
-    
-    var count = 0
     override func touchesBegan(touches: NSSet, withEvent event: UIEvent) {
-        let handNode = self.scene!.rootNode.childNodeWithName("hand", recursively: true)
-        let ik = SCNIKConstraint.inverseKinematicsConstraintWithChainRootNode(self.scene!.rootNode)
+        let robo = self.scene!.rootNode.childNodeWithName("Robo", recursively: true)!
+        let handNode = robo.childNodeWithName("HandBone", recursively: true)!
         
-        ++count
+        let ikc = SCNIKConstraint.inverseKinematicsConstraintWithChainRootNode(robo)
+        handNode.constraints = [ikc]
         
-        println(touches.anyObject())
+        let p = touches.anyObject()!.locationInView(self.view)
+        println("touch location : \(p)") // total arm length :  226, buttom : 160, 410, top : 160, 184
         
-        SCNTransaction.setAnimationDuration(5.0)
-        if count % 2  == 0 {
-            ik.targetPosition = SCNVector3(x: 50, y: 0, z: 0)
-        } else {
-            ik.targetPosition = SCNVector3(x: 35, y: 20, z: 0)
-        }
+        let xTarget = Float((p.x - 160) * 9.4 / 320)
+        let yTarget = -4.2 + -Float((p.y - 410) * 16.7 / 568)
         
-        handNode?.constraints = [ik]
+        println("x : \(xTarget), y : \(yTarget)")
+        
+        SCNTransaction.begin()
+        SCNTransaction.setAnimationDuration(1.0)
+//         ikc.targetPosition = self.scene!.rootNode.convertPosition(SCNVector3(x: xTarget, y: yTarget, z: 0), toNode: nil)
+        ikc.targetPosition = SCNVector3Make(xTarget, yTarget, 0)
+        SCNTransaction.commit()
+    
+        let upperBone = robo.childNodeWithName("UpperBone", recursively: true)!
+        let foreBone = robo.childNodeWithName("ForeBone", recursively: true)!
+        println("upper : \(upperBone.position.x), \(upperBone.position.y), \(upperBone.position.z)")
+        println("fore : \(foreBone.position.x), \(foreBone.position.y), \(foreBone.position.z)")
     }
     
     override func shouldAutorotate() -> Bool {
